4ac00f64d7b5966d417d1daad57784e7c85cc512
Oly1906 <ona26715@gmail.com>
2026-07-28 23:36:04 +0700

    add

--- api/index.php
+++ api/index.php
@@ -1,3 +1,8 @@
 <?php
 
+// បង្កើត folder views ក្នុង /tmp មុនពេល Laravel រត់ (ដើម្បីកុំឲ្យស្ទះ permission)
+if (!is_dir('/tmp/views')) {
+    mkdir('/tmp/views', 0755, true);
+}
+
 require __DIR__ . '/../public/index.php';
